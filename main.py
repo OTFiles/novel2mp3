1aeaf66e8b8a9e57fa017931006cba89f9f2060d
OTFiles <736892504@qq.com>
2024-12-08 14:17:47 +0800

    修改执行顺序为自然排序(2<10)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import sys
 from tqdm import tqdm
+from natsort import natsorted
 
 # 配置参数
 CONFIG = {
@@ -60,23 +61,21 @@ def get_file_count(directory):
     """
     return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
 
-async def process_file(indir, i, voice, rate, volume, padding_length, pbar):
+async def process_file(indir, filename, voice, rate, volume, pbar):
     """
     处理单个文件，生成语音并更新进度条
     :param indir: 输入文件夹路径
-    :param i: 文件编号
+    :param filename: 文件名
     :param voice: 语音类型
     :param rate: 语速
     :param volume: 音量
-    :param padding_length: 文件编号补零长度
     :param pbar: 进度条对象
     """
-    j = "{:0{}d}".format(i, padding_length)
-    with open(f'{indir}/{j}.txt', 'rb') as f:
+    with open(f'{indir}/{filename}', 'rb') as f:
         data = f.read()
         text = data.decode('utf-8')
 
-    output = f'{CONFIG["output_folder"]}/{j}.mp3'
+    output = f'{CONFIG["output_folder"]}/{filename.replace(".txt", ".mp3")}'
     from_time = datetime.datetime.now()
     
     await my_function(text, voice, rate, volume, output)
@@ -84,7 +83,7 @@ async def process_file(indir, i, voice, rate, volume, padding_length, pbar):
     to_time = datetime.datetime.now()
     elapsed_time = (to_time - from_time).total_seconds()
     pbar.update(1)
-    pbar.set_description(f'{j}.txt successed TIME: {elapsed_time:.2f}s')
+    pbar.set_description(f'{filename} successed TIME: {elapsed_time:.2f}s')
 
 async def main():
     """
@@ -100,23 +99,22 @@ async def main():
         print(f'\033[38;2;255;0;0m输入文件夹{indir}中没有可处理的文件!\033[0m')
         sys.exit()
 
-    padding_length = len(str(to))
-    iL = form + 1  # 从已处理文件的下一个开始
+    file_list = natsorted([f for f in os.listdir(indir) if f.endswith('.txt')])
 
     if not os.path.exists('.breakpointfile'):
         with open('.breakpointfile', 'w') as f:
             pass
 
-    ingo = input(f'\033[38;2;0;204;255m程序将从{iL}到{to},是否继续?(Y/n)\033[0m')
+    ingo = input(f'\033[38;2;0;204;255m程序将处理{len(file_list)}个文件,是否继续?(Y/n)\033[0m')
     if ingo == 'n':
         sys.exit()
 
     start_time = datetime.datetime.now()
 
-    with tqdm(total=to - iL + 1, desc="Processing", unit="file") as pbar:
-        for start in range(iL, to + 1, CONFIG['batch_size']):
-            end = min(start + CONFIG['batch_size'] - 1, to)
-            tasks = [process_file(indir, i, CONFIG['voice'], CONFIG['rate'], CONFIG['volume'], padding_length, pbar) for i in range(start, end + 1)]
+    with tqdm(total=len(file_list), desc="Processing", unit="file") as pbar:
+        for start in range(0, len(file_list), CONFIG['batch_size']):
+            end = min(start + CONFIG['batch_size'], len(file_list))
+            tasks = [process_file(indir, file_list[i], CONFIG['voice'], CONFIG['rate'], CONFIG['volume'], pbar) for i in range(start, end)]
             await asyncio.gather(*tasks)
 
     end_time = datetime.datetime.now()
